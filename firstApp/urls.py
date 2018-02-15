ff16977d6cb9b8dd3ce11a351f9bd47d5795a31c
joshua <josh.jesusreigns@gmail.com>
2018-02-15 12:08:12 +0300

    'added_the_interfaces'

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
+    path('upload/', views.upload, name='upload'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('upload_form/', views.upload_form, name='upload_form'),
+    path('books/', views.books, name='books'),
+    path('transactions/', views.transactions, name='transactions'),
+    path('site_admin/', views.site_admin, name='site_admin'),
 
 ]    
